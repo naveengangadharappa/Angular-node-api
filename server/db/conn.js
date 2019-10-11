1516125d687670f54b201ec237d4e568978d6eb8
naveengangadharappa <50243456+naveengangadharappa@users.noreply.github.com>
2019-10-11 13:25:23 +0530

    Update conn.js

--- server/db/conn.js
+++ server/db/conn.js
@@ -2,7 +2,7 @@ const mysql=require('mysql');
 var mysqlconnection=mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:'',
+    password:'root',
     database:'nodejs'
 });
 
